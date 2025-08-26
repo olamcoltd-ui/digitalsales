3b9ca53057db6a9473a24488d4763b4e0d229225
olamcoltd-ui <olamcoltd@gmail.com>
2025-08-26 02:38:40 +0100

    Updated SupportPage.tsx

---
+++ src/pages/SupportPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SupportPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Support Page</h1>
+      <p className="mt-2 text-gray-600">
+        This page will provide support information and FAQs.
+      </p>
+    </div>
+  );
+}
